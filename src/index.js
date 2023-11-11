ae587f4ca464329f4d7a73c38e126eb1ba592524
Rossmaury <142675510+Rossmaury@users.noreply.github.com>
2023-11-11 08:35:54 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.css'; // Import your global styles
 import ToyStoryMemoryGame from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <ToyStoryMemoryGame />
-    </React.StrictMode>,
-    document.getElementById('root')
+   <React.StrictMode>
+       <ToyStoryMemoryGame />
+   </React.StrictMode>,
+   document.getElementById('root')
 );
 
 reportWebVitals();
